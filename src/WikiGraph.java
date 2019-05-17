24c0c168228192e1131ce72b377cea4d0e096b00
erinbastholm <erin.bastholm@gmail.com>
2019-05-17 20:54:03 +0800

    hello i hope this works

--- src/WikiGraph.java
+++ src/WikiGraph.java
@@ -1,25 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// Do you receive this message, over.
 public class WikiGraph implements CITS2200Project {
+
+    private Map<String, String> wikiGraph;
+
+    public WikiGraph() {
+        wikiGraph = new HashMap<>();
+    }
+
     @Override
     public void addEdge(String urlFrom, String urlTo) {
 
     }
+
     @Override
     public int getShortestPath(String urlFrom, String urlTo) {
+
         return 1;
     }
 
     @Override
     public String[] getCenters() {
+
         return new String[0];
     }
 
     @Override
     public String[][] getStronglyConnectedComponents() {
+
         return new String[0][];
     }
 
     @Override
     public String[] getHamiltonianPath() {
+
         return new String[0];
     }
 }
