4e133c0e71b5214655bf8c47bcfc7af8cb7d0da7
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-05-17 13:20:20 +0800

    Initial commit

---
+++ src/WikiGraph.java
@@ -0,0 +1,25 @@
+public class WikiGraph implements CITS2200Project {
+    @Override
+    public void addEdge(String urlFrom, String urlTo) {
+
+    }
+    @Override
+    public int getShortestPath(String urlFrom, String urlTo) {
+        return 1;
+    }
+
+    @Override
+    public String[] getCenters() {
+        return new String[0];
+    }
+
+    @Override
+    public String[][] getStronglyConnectedComponents() {
+        return new String[0][];
+    }
+
+    @Override
+    public String[] getHamiltonianPath() {
+        return new String[0];
+    }
+}
