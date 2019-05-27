2445639895fff222c3a9a3ad61a2b595a37ce3d6
erinbastholm <erin.bastholm@gmail.com>
2019-05-27 12:40:30 +0800

    breadth first search

--- src/WikiGraph.java
+++ src/WikiGraph.java
@@ -1,25 +1,63 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class WikiGraph implements CITS2200Project {
 
-    private Map<String, ArrayList> wikiGraph;
-
+    private HashMap<Integer, ArrayList> wikiGraph = new HashMap<>();
+    private ArrayList vertices = new ArrayList();
     @Override
     public void addEdge(String urlFrom, String urlTo) {
-        ArrayList linkedTo = new ArrayList();
-        if (wikiGraph.containsKey(urlFrom)) {
-            linkedTo = wikiGraph.get(urlFrom);
+        if(!vertices.contains(urlFrom)) {
+            vertices.add(urlFrom);
         }
-        linkedTo.add(urlTo);
-        wikiGraph.put(urlFrom, linkedTo);
+        int from = vertices.indexOf(urlFrom);
+        if(!vertices.contains(urlTo)) {
+            vertices.add(urlTo);
+        }
+        int to = vertices.indexOf(urlTo);
+        ArrayList linkedTo = new ArrayList();
+        if (wikiGraph.containsKey(from)) {
+            linkedTo = wikiGraph.get(from);
+        }
+        linkedTo.add(to);
+        wikiGraph.put(from, linkedTo);
     }
 
     @Override
     public int getShortestPath(String urlFrom, String urlTo) {
+        int shortest = -1;
+        int from = vertices.indexOf(urlFrom);
+        int to = vertices.indexOf(urlTo);
+        int[] colour = new int[vertices.size()];
+        for (int i = 0; i < vertices.size(); i ++){
+            colour[i] = 0;
+        }
+        int[] parent = new int[vertices.size()];
+        for (int i = 0; i < vertices.size(); i ++){
+            parent[i] = -1;
+        }
+        ArrayDeque q = new ArrayDeque();
+        if (wikiGraph.containsKey(from) && vertices.contains(to)) {
+            q.add(from);
+            while (!q.isEmpty()) {
+                Object k = q.pop();
+                for (int i = 0; i < wikiGraph.get(k).size(); i++) {
+                    if (colour[i] == 0) {
+                        q.add(wikiGraph.get(k).get(i));
+                        colour[i] = 1;
+                        parent[i] = (int) k;
+                    }
+                }
+            }
+            if (parent[to] != -1) {
+                int count =
+                while (parent[k] != from) {
 
-        return 1;
+                }
+            }
+        }
+        return shortest;
     }
 
     @Override
